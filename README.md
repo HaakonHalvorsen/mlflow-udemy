a48df137ec68ce09d50af6b1cf93c0eb1d6ecf0c
Håkon Skogland Halvorsen <hakon@alv.no>
2024-02-03 14:34:02 +0100

    Finished logging section

--- README.md
+++ README.md
@@ -4,3 +4,52 @@
 1. Make virtual environment and download all packages in requirements.txt.
 2. Run main.py.
 3. Run `mlflow ui` to access the user interface of MLflow.
+
+## Useful mlflow functions
+### Tracking
+`mlflow.set_tracking_uri(uri)`: Should be set before experiment is created or set. Can be used to set the folder where ml runs will be stored. Default folder name is "mlruns". Can also store runs remotely somewhere, for instance in Databricks or Azure. <br>
+
+`mlflow.get_tracking_uri()`: Get the current tracking URI. This may not correspond to the tracking URI of the currently active run, since the tracking URI can be updated via ``set_tracking_uri``. <br>
+
+## Experiment
+`mlflow.create_experiment(name, artifact_location, tags)`: Create an experiment with a given name and optional artifact location and tags. The machine learning runs will be stored in this experiment. <br>
+
+`mlflow.set_experiment(experiment_name, experiment_id)`: Set the given experiment as the active experiment. The experiment must either be specified by name via experiment_name or by ID via experiment_id. The experiment name and ID cannot both be specified. <br>
+
+`mlflow.get_experiment(experiment_id)`: Retrieve an experiment by experiment_id from the backend store. <br>
+
+### Run
+`mlflow.start_run(experiment_id, ++)`: Start a new MLflow run, setting it as the active run under which metrics and parameters will be logged (mostly during training of ML models). The return value can be used as a context manager within a ``with`` block; otherwise, you must call ``end_run()`` to terminate the current run. <br>
+
+`mlflow.end_run(status)`: End an active MLflow run (if there is one). <br>
+
+`mlflow.active_run()`: Get current active run infomation, like run id and run name from the Run object the function returns. Should be used in a start_run `with` clause or between start_run and end_run. <br>
+
+`mlflow.last_active_run()`: Get the last active run object. Can be used after end_run to get the run information from the already ended run. <br>
+
+### Logging
+
+`mlflow.log_param(key, value)`: Log a parameter (e.g. model hyperparameter) under the current run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.log_params(params)`: Log a batch of params for the current run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.log_metric(key, value)`: Log a metric under the current run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.log_metrics(metrics)`: Log multiple metrics for the current run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.sklearn.log_model(sk_model, artifact_path)`: Log a scikit-learn model as an MLflow artifact for the current run. Produces an MLflow Model. Does not register a model, it's logging it! <br>
+
+`mlflow.log_image(image)`: Log an image as an artifact. The image objects that are supported are `numpy.ndarray` and `PIL.Image.Image`.
+
+`mlflow.log_artifact(local_path)`: Log a local file or directory as an artifact of the currently active run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.log_artifacts(local_dir)`: Log all the contents of a local directory as artifacts of the run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.get_artifact_uri()`: Get the absolute URI of the specified artifact in the currently active run. If `path` is not specified, the artifact root URI of the currently active run will be returned; calls to ``log_artifact`` and ``log_artifacts`` write artifact(s) to subdirectories of the artifact root URI. If no run is active, this method will create a new active run. <br>
+
+`mlflow.set_tag(key, value)`: Set a tag under the current run. If no run is active, this method will create a new active run. <br>
+
+`mlflow.set_tags(tags)`: Log a batch of tags for the current run. If no run is active, this method will create a new active run. <br>
+
+
+
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 import mlflow
 import mlflow.sklearn
+from pathlib import Path
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -36,6 +37,8 @@ if __name__ == "__main__":
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
+    train.to_csv("data/train.csv")
+    test.to_csv("data/test.csv")
 
     # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
@@ -46,27 +49,69 @@ if __name__ == "__main__":
     alpha = args.alpha
     l1_ratio = args.l1_ratio
     
+    # Set tracking URI
+    mlflow.set_tracking_uri(uri="") # <----- Set where the runs should be stored locally or remote
+    #mlflow.get_tracking_uri()
+    
     # Create experiment
-    exp = mlflow.set_experiment(experiment_name="experiment_1")
+    #exp_id = mlflow.create_experiment(
+    #    name="exp_created_artifact",
+    #    tags={"version": "v1", "priority": "p1"},
+    #    artifact_location=Path.cwd().joinpath("myartifacts").as_uri())
+    
+    exp = mlflow.set_experiment(experiment_name="experiment_4")
+    #get_exp = mlflow.get_experiment(exp_id)
+    
+    # Log experiment metadata
+    print(f"Name: {exp.name}")
+    print(f"Experiment_id: {exp.experiment_id}")
+    print(f"Artifact location: {exp.artifact_location}")
+    print(f"Tags: {exp.tags}")
+    print(f"Lifecycle_stage: {exp.lifecycle_stage}")
+    print(f"Creation timestamp: {exp.creation_time}")
     
     # Model training
-    with mlflow.start_run(experiment_id=exp.experiment_id):
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
-
-        predicted_qualities = lr.predict(test_x)
-
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+    mlflow.start_run(experiment_id=exp.experiment_id)
         
-        # Logging
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("mae", mae)
-        mlflow.log_metric("r2", r2)
-        mlflow.sklearn.log_model(lr, "mymodel")
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    
+    # Logging
+    #mlflow.log_param("alpha", alpha)
+    #mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_params({"alpha": alpha, "l1_ratio": l1_ratio})
+    #mlflow.log_metric("rmse", rmse)
+    #mlflow.log_metric("mae", mae)
+    #mlflow.log_metric("r2", r2)
+    mlflow.log_metrics({"rmse": rmse, "mae": mae, "r2": r2})
+    mlflow.sklearn.log_model(lr, "mymodel")
+    
+    # Log the data
+    mlflow.log_artifacts("data/")
+    
+    # Get information from the active run
+    active_run = mlflow.active_run()
+    print(f"Active run id is {active_run.info.run_id}")
+    print(f"Active run name is {active_run.info.run_name}")
+
+    # Set tags
+    mlflow.set_tag("release.version", 0.2)
+    mlflow.set_tags({"environment": "dev", "priority": "p1"})
+
+    # Get artifact uri
+    artifacts_uri = mlflow.get_artifact_uri()
+    print(f"The artifact path is: {artifacts_uri}")
+    
+    # End current mlflow run
+    mlflow.end_run()
+    
+    # Get information from the last active run
+    last_run = mlflow.last_active_run()
+    print(f"Last active run id is {last_run.info.run_id}")
+    print(f"Last active run name is {last_run.info.run_name}")
