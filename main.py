38d7c2369302eae3e022660bae1bab0ada039571
Håkon Skogland Halvorsen <haakonshalvorsen@gmail.com>
2024-02-06 16:34:49 +0100

    [#8] Added code to build customized models in mlflow

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from sklearn.linear_model import ElasticNet
 import mlflow
 import mlflow.sklearn
 from mlflow.models.signature import infer_signature
+import sklearn
+import joblib
+import cloudpickle
+import os
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -37,8 +41,15 @@ if __name__ == "__main__":
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
-    train.to_csv("data/train.csv")
-    test.to_csv("data/test.csv")
+    
+    data_dir = 'red-wine-data'
+    
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    
+    data.to_csv(data_dir + '/data.csv')
+    data.to_csv(data_dir + '/train.csv')
+    data.to_csv(data_dir + '/test.csv')
 
     # The predicted column is "quality" which is a scalar from [3, 9]
     train_x = train.drop(["quality"], axis=1)
@@ -59,7 +70,7 @@ if __name__ == "__main__":
     #    tags={"version": "v1", "priority": "p1"},
     #    artifact_location=Path.cwd().joinpath("myartifacts").as_uri())
     
-    exp = mlflow.set_experiment(experiment_name="experiment_signature")
+    exp = mlflow.set_experiment(experiment_name="experiment_custom_sklearn")
     #get_exp = mlflow.get_experiment(exp_id)
     
     # Log experiment metadata
@@ -99,17 +110,8 @@ if __name__ == "__main__":
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
     
-    signature = infer_signature(test_x, predicted_qualities)
-    input_example = {
-        "columns": np.array(test_x.columns),
-        "data": np.array(test_x.values)
-    }
-    
     # Log the data
     mlflow.log_artifact("data/red-wine-quality.csv")
-    
-    # Log model
-    mlflow.sklearn.save_model(lr, "model", signature=signature, input_example=input_example)
     
     # Get information from the active run
     active_run = mlflow.active_run()
@@ -119,6 +121,53 @@ if __name__ == "__main__":
     # Set tags
     mlflow.set_tag("release.version", 0.2)
     mlflow.set_tags({"environment": "dev", "priority": "p1"})
+    
+    sklearn_model_path = "sklearn_model.pkl"
+    joblib.dump(lr, sklearn_model_path)
+    artifacts = {
+        "sklearn_model": sklearn_model_path,
+        "data": data_dir
+    }
+    
+    class SklearnWrapper(mlflow.pyfunc.PythonModel):
+        def load_context(self, context):
+            self.sklearn_model = joblib.load(context.artifacts["sklearn_model"])
+            
+        def predict(self, context, model_input):
+            return self.sklearn_model.predict(model_input.values)
+    
+    # Create a Conda environment for the new MLflow Model that contains all necessary dependencies.
+    conda_env = {
+        "channels": ["defaults"],
+        "dependencies": [
+            "python={}".format(3.12),
+            "pip",
+            {
+                "pip": [
+                    "mlflow=={}".format(mlflow.__version__),
+                    "scikit-learn=={}".format(sklearn.__version__),
+                    "cloudpickle=={}".format(cloudpickle.__version__),
+                ],
+            },
+        ],
+        "name": "sklearn_env",
+    }
+    
+    mlflow.pyfunc.log_model(
+        artifact_path="sklearn_mlflow_pyfunc",
+        python_model=SklearnWrapper(),
+        artifacts=artifacts,
+        code_path=["main.py"],
+        conda_env=conda_env
+    )
+    
+    ld = mlflow.pyfunc.load_model(model_uri="runs:/"+active_run.info.run_id+"/sklearn_mlflow_pyfunc")
+    predicted_qualities = ld.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    # print metrics
+    print(" RMSE_test: %s" % rmse)
+    print(" MAE_test: %s" % mae)
+    print(" R2_test: %s" % r2)
 
     # Get artifact uri
     artifacts_uri = mlflow.get_artifact_uri()
